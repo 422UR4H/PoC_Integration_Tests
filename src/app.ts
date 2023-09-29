fb45a79cecb7e29a6eb965d35907cfb8a6fb8c67
422UR4H <tiagosindra@id.uff.br>
2023-09-29 17:00:03 -0300

    :recycle: refactor/server-index

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const port = process.env.PORT || 5000;
 const app = express();
 app
     .use(json())
     .use(router)
-    .use(errorHandler)
-    .listen(port, () => console.log(`Server is running on port ${port}`));
+    .use(errorHandler);
+
+export default app;
